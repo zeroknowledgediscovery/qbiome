cb3a38ffa535e5b041318b81ccab2cf55d573e43
ishanuc <ishanu.chattopadhyay@gmail.com>
2021-05-17 23:10:22 -0500

    doc_bash_added

---
+++ qbiome/qbiome.py
@@ -0,0 +1,6 @@
+class qbiome(object):
+    """Microbiome Analysis Powered By Recursive Quasi-species Networks: Uncovering rules of organization, competition, succession and exploitation
+    """
+
+    def __init__(self):
+        pass
